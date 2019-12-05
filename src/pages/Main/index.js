ba95fd109332659b2df4fb821cdd22d537c0fcc5
Gabriel <gabrielagrelli@hotmail.com>
2019-12-04 23:02:57 -0300

    Listing products index

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,76 @@
 import React, {Component} from 'react';
+import {FlatList} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
+import {formatPrice} from '../../utils/format';
 
-import {View} from 'react-native';
-
-// import { Container } from './styles';
+import {
+  Container,
+  Product,
+  ProductImage,
+  ProductTitle,
+  ProductPrice,
+  AddButton,
+  ProductAmount,
+  ProductAmountText,
+  AddButtonText,
+} from './styles';
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts = async () => {
+    const response = await api.get('/products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      proceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({products: data});
+  };
+
+  renderProduct = ({item}) => {
+    return (
+      <Product key={item.id}>
+        <ProductImage source={{uri: item.image}} />
+        <ProductTitle>{item.title}</ProductTitle>
+        <ProductPrice>{item.price}</ProductPrice>
+        <AddButton>
+          <ProductAmount>
+            <Icon name="add-shopping-cart" color="#fff" size={20} />
+            <ProductAmountText>2</ProductAmountText>
+          </ProductAmount>
+          <AddButtonText>ADICIONAR</AddButtonText>
+        </AddButton>
+      </Product>
+    );
+  };
+
   render() {
-    return <View />;
+    const {products} = this.state;
+
+    return (
+      <Container>
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          data={products}
+          extraData={this.props}
+          keyExtractor={item => String(item.id)}
+          renderItem={this.renderProduct}
+        />
+      </Container>
+    );
   }
 }
 
